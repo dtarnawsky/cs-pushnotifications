0ad8f28d909ee126987d501293d93dc1f58707f7
Damian Tarnawsky <damian@ionic.io>
2022-01-07 13:38:33 -0800

    Add csharp-backend

---
+++ csharp-backend/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Azure.NotificationHubs;
+using Microsoft.Azure.NotificationHubs.Messaging;
+
+
+namespace notify
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            // Getting connection key from the new resource
+            var hubName = "cs-ionic-notifications";
+            //var primaryConnectionString = ""; // Get this from Azure Portal -> Manage -> Access Policies                        
+            var deviceToken = "BF19A750D18CD9D8BCF31D338354AA99CF42AB129CC662142A3D8A21579A55B6";
+            if (args.Length > 0)
+            {
+                deviceToken = args[0];
+            }
+            var client = NotificationHubClient.CreateClientFromConnectionString(primaryConnectionString, hubName);
+
+
+            await RegisterAppleDevice(client, deviceToken);
+            //await CreateAndDeleteInstallationAsync(nhClient);
+            //await CreateAndDeleteRegistrationAsync(nhClient);
+        }
+
+        private static async Task RegisterAppleDevice(NotificationHubClient client, string token)
+        {
+            Console.WriteLine($"Register Apple Device {token}");
+            var desc = await client.CreateAppleNativeRegistrationAsync(token);
+            Console.WriteLine($"Register Apple Device Result:  {desc.Serialize()}");
+        }
+
+        private static async Task CreateAndDeleteRegistrationAsync(NotificationHubClient nhClient)
+        {
+            var registrationId = await nhClient.CreateRegistrationIdAsync();
+            var registrationDescr = await nhClient.CreateFcmNativeRegistrationAsync(registrationId);
+            Console.WriteLine($"Created FCM registration {registrationDescr.FcmRegistrationId}");
+
+            var allRegistrations = await nhClient.GetAllRegistrationsAsync(1000);
+            foreach (var regFromServer in allRegistrations)
+            {
+                if (regFromServer.RegistrationId == registrationDescr.RegistrationId)
+                {
+                    Console.WriteLine($"Found FCM registration {registrationDescr.FcmRegistrationId}");
+                    break;
+                }
+            }
+
+            //registrationDescr = await nhClient.GetRegistrationAsync<FcmRegistrationDescription>(registrationId);
+            //Console.WriteLine($"Retrieved FCM registration {registrationDescr.FcmRegistrationId}");
+
+            await nhClient.DeleteRegistrationAsync(registrationDescr);
+            Console.WriteLine($"Deleted FCM registration {registrationDescr.FcmRegistrationId}");
+        }
+
+        private static async Task CreateAndDeleteInstallationAsync(NotificationHubClient nhClient)
+        {
+            // Register some fake devices
+            var fcmDeviceId = Guid.NewGuid().ToString();
+            var fcmInstallation = new Installation
+            {
+                InstallationId = fcmDeviceId,
+                Platform = NotificationPlatform.Fcm,
+                PushChannel = fcmDeviceId,
+                PushChannelExpired = false,
+                Tags = new[] { "fcm" }
+            };
+            await nhClient.CreateOrUpdateInstallationAsync(fcmInstallation);
+
+            while (true)
+            {
+                try
+                {
+                    var installationFromServer = await nhClient.GetInstallationAsync(fcmInstallation.InstallationId);
+                    break;
+                }
+                catch (MessagingEntityNotFoundException)
+                {
+                    // Wait for installation to be created
+                    await Task.Delay(1000);
+                }
+            }
+            Console.WriteLine($"Created FCM installation {fcmInstallation.InstallationId}");
+            await nhClient.DeleteInstallationAsync(fcmInstallation.InstallationId);
+            while (true)
+            {
+                try
+                {
+                    var installationFromServer = await nhClient.GetInstallationAsync(fcmInstallation.InstallationId);
+                    await Task.Delay(1000);
+                }
+                catch (MessagingEntityNotFoundException)
+                {
+                    Console.WriteLine($"Deleted FCM installation {fcmInstallation.InstallationId}");
+                    break;
+                }
+            }
+        }
+    }
+}
