dac0ef7fa06a25ea6c6eb8414d38b0ebb0e2cbaa
dtarnawsky <damian@ionic.io>
2023-05-17 06:08:54 -0700

    feat: update to Capacitor 5

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,7 +4,6 @@ const config: CapacitorConfig = {
   appId: 'io.ionic.cs.pushnotifications',
   appName: 'cs-push-notifications',
   webDir: 'www',
-  bundledWebRuntime: false,
   plugins: {
     PushNotifications: {
       presentationOptions: ['badge', 'sound', 'alert'],
